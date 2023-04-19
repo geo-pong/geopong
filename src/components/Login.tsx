e62d8b400cd3a3f60bbb7d4043e625104abb70c5
Christopher Suzukida <chris.suzukida@gmail.com>
2023-04-19 00:02:39 -0700

    Modify Login component for Google auth

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+declare const google: any;
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const handleLogin = () => {
+  const handleLogin = (response:gapi.auth2.GoogleUser) => {
+    // const credential = response.credential;
+    const id_token = response.getAuthResponse().id_token;
     navigate('/Pong');
   }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "897538926324-ujej9nnvaq2hohplofks4g53jne005gk.apps.googleusercontent.com",
+      callback: handleLogin,
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById('sign-in-div'),
+      { theme: 'outline', size: 'large'}
+    );
+  }, []);
 
   return (
     <div className="login">
